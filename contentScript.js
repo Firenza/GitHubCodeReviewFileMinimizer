f0c2a270cf3a15408c0dcff56339f94f1ba3b16d
Chad Oftedahl <chad.oftedahl@chrobinson.com>
2017-12-18 08:27:07 -0600

    Initial mutation observer testing

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,66 @@
-$('div .file-header').each(function (index) {
+var targetNodes = $(".repository-content");
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var myObserver = new MutationObserver(mutationHandler);
+var obsConfig = { childList: true, characterData: true, attributes: true, subtree: true };
+var inMutation = false;
 
-    let fileName = $(this).find('div.file-info').find('a').attr('title');
-    console.log(fileName);
 
-    if (fileName.endsWith('.csproj') || fileName.endsWith('.sln') || fileName.endsWith('packages.config')){
-        $(this).find('button').click();
-    }
-   
+//--- Add a target node to the observer. Can only add one node at a time.
+targetNodes.each(function () {
+    myObserver.observe(this, obsConfig);
 });
+
+function mutationHandler(mutationRecords) {
+    
+    console.info("mutationHandler:");
+    console.log("in Mutation = " + inMutation);
+    inMutation = true;
+
+    mutationRecords.forEach(function (mutation) {
+        if (mutation.type == "childList") {
+            if (typeof mutation.addedNodes == "object") {
+                console.log(JSON.stringify(mutation.addedNodes));
+
+                console.log(mutation.addedNodes);
+                console.log("SOME " + _.some(mutation.addedNodes, '#text'));
+
+                mutation.addedNodes.forEach(element => {
+
+                    console.log(typeof element);
+                    console.log(JSON.stringify(element));
+                });
+
+                console.log($(mutation.addedNodes).first())
+                console.log($(mutation.addedNodes).find('text'));
+                
+                if ($(mutation.addedNodes).find('text').length){
+                    collapseDiffs();
+                }
+            }
+        }
+    });
+
+    inMutation = false;
+}
+
+function collapseDiffs() {
+
+
+    $('div .file-header').each(function (index) {
+
+        let fileName = $(this).find('div.file-info').find('a').attr('title');
+        console.log(fileName);
+
+        if (fileName.endsWith('.csproj') || fileName.endsWith('.sln') || fileName.endsWith('packages.config')) {
+            // Check to see if this diff has already been collapsed
+            let $button = $(this).find('button');
+            let isButtonInExpandedMode = $button.attr('aria-expanded');
+
+            if (isButtonInExpandedMode){
+                console.log('clicking button');
+                $button.click();
+            }
+        }
+
+    });
+}
