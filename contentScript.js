f93a802c83185d4edd071356fe5ea464913e1605
Chad Oftedahl <chad.oftedahl@chrobinson.com>
2017-12-19 07:43:09 -0600

    Got things mostly working

--- contentScript.js
+++ contentScript.js
@@ -11,9 +11,9 @@ function mutationHandler(mutationRecords) {
     mutationRecords.forEach(function (mutation) {
         // Only care about this event of stuff got added
         if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            
+
             collapseDiffs();
-            
+
             // There is only one delayed request to get more diff data so we only need
             // to watch for one mutation
             observer.disconnect();
@@ -21,11 +21,22 @@ function mutationHandler(mutationRecords) {
     });
 }
 
+let key = "fileDiffToCollapseRegex";
+let fileDifToCollapseRegex = undefined;
+
+chrome.storage.sync.get(key, (items) => {
+    let fileDifToCollapseRegexString = items[key];
+
+    if (fileDifToCollapseRegexString !== undefined){
+        fileDifToCollapseRegex = RegExp(fileDifToCollapseRegexString);
+    }
+});
+
 function collapseDiffs() {
     $('div .file-header').each(function (index) {
         let fileName = $(this).find('div.file-info').find('a').attr('title');
 
-        if (fileName.endsWith('.orig') || fileName.endsWith('.csproj') || fileName.endsWith('.sln') || fileName.endsWith('packages.config')) {
+        if (fileDifToCollapseRegex !== undefined && fileDifToCollapseRegex.test(fileName)) {
 
             // Check to see if this diff has already been collapsed
             let $button = $(this).find('button');
