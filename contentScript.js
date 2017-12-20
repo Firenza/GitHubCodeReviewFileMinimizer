5a2a3d7632620764a9e5034664cb934d2a29b7c7
Chad Oftedahl <chadoftedahl@gmail.com>
2017-12-20 08:42:16 -0600

    Put URL matching in manifest

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,15 @@
+// Figure out whether or not the script is running in developer mode
+// the 'update_url' manifest key is added when an extension is uploaded to the 
+// chrome web store
+const IS_DEV_MODE = !('update_url' in chrome.runtime.getManifest());
+const logDev = (message) => {
+    if (IS_DEV_MODE) {
+        console.log(message);
+    }
+}
+
+logDev("Executing Content Script");
+
 // Setup mutation observer
 let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 let observer = new MutationObserver(mutationHandler);
@@ -11,6 +23,7 @@ function mutationHandler(mutationRecords) {
     mutationRecords.forEach(function (mutation) {
         // Only care about this event of stuff got added
         if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+            logDev("Mutation handler triggered");
 
             collapseDiffs();
 
@@ -27,14 +40,13 @@ function collapseDiffs() {
     $('div .file-header').each(function (index) {
         let fileName = $(this).find('div.file-info').find('a').attr('title');
 
-        
-       if (fileDifToCollapseRegex !== undefined && fileDifToCollapseRegex.test(fileName)) {
+        if (fileDifToCollapseRegex !== undefined && fileDifToCollapseRegex.test(fileName)) {
             // Check to see if this diff has already been collapsed
             let $button = $(this).find('button');
             let isButtonInExpandedMode = $button.attr('aria-expanded');
 
             if (isButtonInExpandedMode) {
-                
+
                 // Directly doing the css updates that the button click does works more reliably that mimicing
                 // a button click so do that
                 $(this).parent().attr('class', 'file js-file js-details-container Details show-inline-notes js-transitionable open Details--on');
