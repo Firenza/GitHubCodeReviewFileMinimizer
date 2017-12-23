32cceb37eb230c95ae8475a4cb9c4d4418629ea4
Chad Oftedahl <chadoftedahl@gmail.com>
2017-12-23 12:35:27 -0600

    Updated jquery script name

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@ var pullRequestFilePageUrlRegex = new RegExp('github[^\/]*\/[^\/]+\/[^\/]+\/pull
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "complete" && pullRequestFilePageUrlRegex.test(tab.url)) {
         console.log("Background Script URL match!");
-        chrome.tabs.executeScript(null, {file: "jquery-3.2.1.slim.min.js"});
+        chrome.tabs.executeScript(null, {file: "jquery.min.js"});
         chrome.tabs.executeScript(null, {file: "contentscript.js"});
     }
 });
