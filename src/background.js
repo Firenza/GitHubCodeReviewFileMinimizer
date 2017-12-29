da2b141d6327f6476ac409936f13fb371dfcd1f2
Firenza <chadoftedahl@gmail.com>
2017-12-29 15:50:59 -0600

    added debounce around condition update

--- src/background.js
+++ src/background.js
@@ -11,15 +11,22 @@ ga('create', 'UA-36364890-2', 'auto');
 // Disable this check so requests from the chrome-extension:// host get sent
 ga('set', 'checkProtocolTask', function(){ /* nothing */ });
 
+let debouncedDiffConditionChangeHandlers = [];
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
         if (request.type === "diffCollapsed"){
+            let label = request.payload.diffSettingThatMatched.matchType;
+
+            if (request.payload.diffSettingThatMatched.matchString && request.payload.diffSettingThatMatched.matchString.trim()) {
+                label += " | " + request.payload.diffSettingThatMatched.matchString
+            }
 
             ga('send', 'event', {
                 eventCategory: 'File Diff Collapse',
                 eventAction: 'file diff setting triggered',
-                eventLabel: request.payload.diffSettingThatMatched.matchType + ' | ' + request.payload.diffSettingThatMatched.matchString,
+                eventLabel: label,
                 dimension1: request.payload.diffSettingThatMatched.matchType,
                 dimension2: request.payload.diffSettingThatMatched.matchString,
                 metric1: request.payload.linesInDiff
@@ -27,27 +34,59 @@ chrome.runtime.onMessage.addListener(
         }
 
         if (request.type === 'diffConditionDeleted') {
+            let label = request.payload.diffToDelete.matchType;
+
+            if (request.payload.diffToDelete.matchString && request.payload.diffToDelete.matchString.trim()) {
+                label += " | " + request.payload.diffToDelete.matchString
+            }
+
             ga('send', 'event', {
                 eventCategory: 'File Diff Interaction',
-                eventAction: 'delete condition click',
-                eventLabel: request.payload.diffToDelete.matchType + ' | ' + request.payload.diffToDelete.diffToDelete.matchString,
-                dimension1: request.payload.diffToDelete.diffToDelete.matchtype,
-                dimension2: request.payload.diffToDelete.diffToDelete.matchString
+                eventAction: 'condition deleted',
+                eventLabel: label,
+                dimension1: request.payload.diffToDelete.matchtype,
+                dimension2: request.payload.diffToDelete.matchString
             });
         }
 
         if (request.type === 'diffConditionAdded') {
             ga('send', 'event', {
                 eventCategory: 'File Diff Interaction',
-                eventAction: 'add new condition click'
+                eventAction: 'condition added'
             });
         }
 
         if (request.type === 'diffConditionUpdated') {
-            ga('send', 'event', {
-                eventCategory: 'File Diff Interaction',
-                eventAction: 'add new condition click'
-            });
+            let diffConditionId = request.payload.updatedDiffCondition.id;
+
+            let deboundedDiffConditionHandler = debouncedDiffConditionChangeHandlers[diffConditionId]
+
+            if (!deboundedDiffConditionHandler) {
+                deboundedDiffConditionHandler = _.debounce((updatedDiffCondition) => {
+
+                    let label = updatedDiffCondition.matchType;
+
+                    if (updatedDiffCondition.matchString && updatedDiffCondition.matchString.trim()) {
+                        label += " | " + updatedDiffCondition.matchString
+                    }
+
+                    ga('send', 'event', {
+                        eventCategory: 'File Diff Interaction',
+                        eventAction: 'condition updated',
+                        eventLabel: updatedDiffCondition.matchType + ' | ' + updatedDiffCondition.matchString,
+                        dimension1: updatedDiffCondition.matchType,
+                        dimension2: updatedDiffCondition.matchString
+                    });
+                }, 2000);
+
+                // Store a seperate debounced method for each condition so no updates are sent to google analtyics
+                // until any given field stays unchanged for the debounce period.  The debounce period for saving the 
+                // updated data needs to be short to make sure the data is saved before the extension UI is closed which 
+                // means if we sent data to google analtyics without an additional delay a lot of uneeded data would be sent
+                debouncedDiffConditionChangeHandlers[diffConditionId] = deboundedDiffConditionHandler;
+            }
+
+            deboundedDiffConditionHandler(request.payload.updatedDiffCondition);
         }
 
         if (request.type === 'extensionUILoaded') {
