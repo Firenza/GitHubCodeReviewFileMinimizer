a7032d87b9ec9eebf0187fbd1fff8411f24f7e8a
Chad Oftedahl <chadoftedahl@gmail.com>
2017-12-31 12:29:40 -0600

    Stop copying unchanged files

--- webpack.config.js
+++ webpack.config.js
@@ -33,15 +33,7 @@ module.exports = {
             { from: 'src/images/*', to: 'images', flatten: true},
             { from: 'node_modules/jquery/dist/jquery.min.js', flatten: true},
             { from: 'node_modules/lodash/lodash.min.js', flatten: true}
-        ], {
-                ignore: [
-                ],
-
-                // By default, we only copy modified files during
-                // a watch or webpack-dev-server build. Setting this
-                // to `true` copies all files.
-                copyUnmodified: true
-            }),
+        ]),
         new ChromeExtensionReloader(),
     ]
 }
